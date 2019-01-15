3af97503b4c9e028029e2e1d633ccf97fe2abfde
Lanny Wang <09legend.wang@gmail.com>
2019-01-15 17:15:46 +0000

    initial setup

--- app.js
+++ app.js
@@ -5,40 +5,69 @@ var bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
-var campgrounds = [
-        {name: "Salmon Creek", image: "https://farm9.staticflickr.com/8442/7962474612_bf2baf67c0.jpg"},
-        {name: "Granite Hill", image: "https://farm1.staticflickr.com/60/215827008_6489cd30c3.jpg"},
-        {name: "Mountain Goat's Rest", image: "https://farm7.staticflickr.com/6057/6234565071_4d20668bbd.jpg"},
-        {name: "Salmon Creek", image: "https://farm9.staticflickr.com/8442/7962474612_bf2baf67c0.jpg"},
-        {name: "Granite Hill", image: "https://farm1.staticflickr.com/60/215827008_6489cd30c3.jpg"},
-        {name: "Mountain Goat's Rest", image: "https://farm7.staticflickr.com/6057/6234565071_4d20668bbd.jpg"},
-        {name: "Salmon Creek", image: "https://farm9.staticflickr.com/8442/7962474612_bf2baf67c0.jpg"},
-        {name: "Granite Hill", image: "https://farm1.staticflickr.com/60/215827008_6489cd30c3.jpg"},
-        {name: "Mountain Goat's Rest", image: "https://farm7.staticflickr.com/6057/6234565071_4d20668bbd.jpg"}
+var movies = [
+        {"title":"Call Me by Your Name",
+        "genre":"Drama, Romance",
+        "poster":"https://m.media-amazon.com/images/M/MV5BNDk3NTEwNjc0MV5BMl5BanBnXkFtZTgwNzYxNTMwMzI@._V1_SX300.jpg"},
+        { "title": "California",
+        "genre": "Romance, Western",
+        "poster": "https://m.media-amazon.com/images/M/MV5BNzhhMmM4M2ItMWZhOS00NDUzLTk5ZTAtMjVkM2NiMGMyYWQwXkEyXkFqcGdeQXVyNjE5MjUyOTM@._V1_SX300.jpg"},
+        { "title": "Happy",
+         "genre": "Documentary, Drama, Family",
+         "poster": "https://images-na.ssl-images-amazon.com/images/M/MV5BMjI1NTg1MzcwM15BMl5BanBnXkFtZTcwMjA2OTU4NQ@@._V1_SX300.jpg"},
+                 {"title":"Call Me by Your Name",
+        "genre":"Drama, Romance",
+        "poster":"https://m.media-amazon.com/images/M/MV5BNDk3NTEwNjc0MV5BMl5BanBnXkFtZTgwNzYxNTMwMzI@._V1_SX300.jpg"},
+        { "title": "California",
+        "genre": "Romance, Western",
+        "poster": "https://m.media-amazon.com/images/M/MV5BNzhhMmM4M2ItMWZhOS00NDUzLTk5ZTAtMjVkM2NiMGMyYWQwXkEyXkFqcGdeQXVyNjE5MjUyOTM@._V1_SX300.jpg"},
+        { "title": "Happy",
+         "genre": "Documentary, Drama, Family",
+         "poster": "https://images-na.ssl-images-amazon.com/images/M/MV5BMjI1NTg1MzcwM15BMl5BanBnXkFtZTcwMjA2OTU4NQ@@._V1_SX300.jpg"},
+                 {"title":"Call Me by Your Name",
+        "genre":"Drama, Romance",
+        "poster":"https://m.media-amazon.com/images/M/MV5BNDk3NTEwNjc0MV5BMl5BanBnXkFtZTgwNzYxNTMwMzI@._V1_SX300.jpg"},
+        { "title": "California",
+        "genre": "Romance, Western",
+        "poster": "https://m.media-amazon.com/images/M/MV5BNzhhMmM4M2ItMWZhOS00NDUzLTk5ZTAtMjVkM2NiMGMyYWQwXkEyXkFqcGdeQXVyNjE5MjUyOTM@._V1_SX300.jpg"},
+        { "title": "Happy",
+         "genre": "Documentary, Drama, Family",
+         "poster": "https://images-na.ssl-images-amazon.com/images/M/MV5BMjI1NTg1MzcwM15BMl5BanBnXkFtZTcwMjA2OTU4NQ@@._V1_SX300.jpg"},
+                {"title":"Call Me by Your Name",
+        "genre":"Drama, Romance",
+        "poster":"https://m.media-amazon.com/images/M/MV5BNDk3NTEwNjc0MV5BMl5BanBnXkFtZTgwNzYxNTMwMzI@._V1_SX300.jpg"},
+        { "title": "California",
+        "genre": "Romance, Western",
+        "poster": "https://m.media-amazon.com/images/M/MV5BNzhhMmM4M2ItMWZhOS00NDUzLTk5ZTAtMjVkM2NiMGMyYWQwXkEyXkFqcGdeQXVyNjE5MjUyOTM@._V1_SX300.jpg"},
+        { "title": "Happy",
+         "genre": "Documentary, Drama, Family",
+         "poster": "https://images-na.ssl-images-amazon.com/images/M/MV5BMjI1NTg1MzcwM15BMl5BanBnXkFtZTcwMjA2OTU4NQ@@._V1_SX300.jpg"}
+    
 ];
     
 app.get("/", function(req, res){
     res.render("landing");
 });
 
-app.get("/campgrounds", function(req, res){
-    res.render("campgrounds",{campgrounds:campgrounds});
+app.get("/movies", function(req, res){
+    res.render("movies",{movies:movies});
 });
 
-app.post("/campgrounds", function(req, res){
+app.post("/movies", function(req, res){
     // get data from form and add to campgrounds array
-    var name = req.body.name;
-    var image = req.body.image;
-    var newCampground = {name: name, image: image}
-    campgrounds.push(newCampground);
-    //redirect back to campgrounds page
-    res.redirect("/campgrounds");
+    var title = req.body.title;
+    var genre = req.body.genre;
+    var poster = req.body.poster;
+    var newMovie = {title: title, genre: genre, poster: poster}
+    movies.push(newMovie);
+    //redirect back to movies page
+    res.redirect("/movies");
 });
 
-app.get("/campgrounds/new", function(req, res){
+app.get("/movies/new", function(req, res){
    res.render("new.ejs"); 
 });
 
-app.listen(process.env.PORT, process.env.IP, function(){
-   console.log("The YelpCamp Server Has Started!");
+app.listen(process.env.PORT || 3000, process.env.IP, function(){
+   console.log("The view-your-movies Server Has Started!");
 });
