ec96f8aa0faed1cf721cbc59365ef8b025a277d2
Lanny Wang <09legend.wang@gmail.com>
2019-01-15 19:00:14 +0000

    created a mongodb, connected through mongoose, for  data persistence

--- app.js
+++ app.js
@@ -1,56 +1,92 @@
-var express = require("express");
-var app = express();
-var bodyParser = require("body-parser");
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const mongoose = require('mongoose');
+
+//connect to our mongodb rate_movie databsae;
+mongoose.connect("mongodb://localhost:27017/rate_movies", {useNewUrlParser: true});
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
+//SCHEMA SET UP: make a model "Movie" using the "movieSchema" schema
+var movieSchema = new mongoose.Schema({
+    title: String,
+    genre: String,
+    poster: String
+});
+
+var Movie = mongoose.model("Movie", movieSchema);
+
+//example of inserting an object into the "Movie" model in the db, inserted four movies
+// Movie.create(
+//     {title: "Happy",
+//          genre: "Documentary, Drama, Family",
+//          poster: "https://images-na.ssl-images-amazon.com/images/M/MV5BMjI1NTg1MzcwM15BMl5BanBnXkFtZTcwMjA2OTU4NQ@@._V1_SX300.jpg" }, function(err, movie){
+//         if(err){
+//             console.log(err);
+//         } else {
+//             console.log("newly created movie :");
+//             console.log(movie);
+//         }
+//     });
+
+
+
 var movies = [
-        {"title":"Call Me by Your Name",
-        "genre":"Drama, Romance",
-        "poster":"https://m.media-amazon.com/images/M/MV5BNDk3NTEwNjc0MV5BMl5BanBnXkFtZTgwNzYxNTMwMzI@._V1_SX300.jpg"},
-        { "title": "California",
-        "genre": "Romance, Western",
-        "poster": "https://m.media-amazon.com/images/M/MV5BNzhhMmM4M2ItMWZhOS00NDUzLTk5ZTAtMjVkM2NiMGMyYWQwXkEyXkFqcGdeQXVyNjE5MjUyOTM@._V1_SX300.jpg"},
-        { "title": "Happy",
-         "genre": "Documentary, Drama, Family",
-         "poster": "https://images-na.ssl-images-amazon.com/images/M/MV5BMjI1NTg1MzcwM15BMl5BanBnXkFtZTcwMjA2OTU4NQ@@._V1_SX300.jpg"},
-                 {"title":"Call Me by Your Name",
-        "genre":"Drama, Romance",
-        "poster":"https://m.media-amazon.com/images/M/MV5BNDk3NTEwNjc0MV5BMl5BanBnXkFtZTgwNzYxNTMwMzI@._V1_SX300.jpg"},
-        { "title": "California",
-        "genre": "Romance, Western",
-        "poster": "https://m.media-amazon.com/images/M/MV5BNzhhMmM4M2ItMWZhOS00NDUzLTk5ZTAtMjVkM2NiMGMyYWQwXkEyXkFqcGdeQXVyNjE5MjUyOTM@._V1_SX300.jpg"},
-        { "title": "Happy",
-         "genre": "Documentary, Drama, Family",
-         "poster": "https://images-na.ssl-images-amazon.com/images/M/MV5BMjI1NTg1MzcwM15BMl5BanBnXkFtZTcwMjA2OTU4NQ@@._V1_SX300.jpg"},
-                 {"title":"Call Me by Your Name",
-        "genre":"Drama, Romance",
-        "poster":"https://m.media-amazon.com/images/M/MV5BNDk3NTEwNjc0MV5BMl5BanBnXkFtZTgwNzYxNTMwMzI@._V1_SX300.jpg"},
-        { "title": "California",
-        "genre": "Romance, Western",
-        "poster": "https://m.media-amazon.com/images/M/MV5BNzhhMmM4M2ItMWZhOS00NDUzLTk5ZTAtMjVkM2NiMGMyYWQwXkEyXkFqcGdeQXVyNjE5MjUyOTM@._V1_SX300.jpg"},
-        { "title": "Happy",
-         "genre": "Documentary, Drama, Family",
-         "poster": "https://images-na.ssl-images-amazon.com/images/M/MV5BMjI1NTg1MzcwM15BMl5BanBnXkFtZTcwMjA2OTU4NQ@@._V1_SX300.jpg"},
-                {"title":"Call Me by Your Name",
-        "genre":"Drama, Romance",
-        "poster":"https://m.media-amazon.com/images/M/MV5BNDk3NTEwNjc0MV5BMl5BanBnXkFtZTgwNzYxNTMwMzI@._V1_SX300.jpg"},
-        { "title": "California",
-        "genre": "Romance, Western",
-        "poster": "https://m.media-amazon.com/images/M/MV5BNzhhMmM4M2ItMWZhOS00NDUzLTk5ZTAtMjVkM2NiMGMyYWQwXkEyXkFqcGdeQXVyNjE5MjUyOTM@._V1_SX300.jpg"},
-        { "title": "Happy",
-         "genre": "Documentary, Drama, Family",
-         "poster": "https://images-na.ssl-images-amazon.com/images/M/MV5BMjI1NTg1MzcwM15BMl5BanBnXkFtZTcwMjA2OTU4NQ@@._V1_SX300.jpg"}
-    
+        {title:"Call Me by Your Name",
+        genre:"Drama, Romance",
+        poster:"https://m.media-amazon.com/images/M/MV5BNDk3NTEwNjc0MV5BMl5BanBnXkFtZTgwNzYxNTMwMzI@._V1_SX300.jpg"},
+        {title: "California",
+        genre: "Romance, Western",
+        poster: "https://m.media-amazon.com/images/M/MV5BNzhhMmM4M2ItMWZhOS00NDUzLTk5ZTAtMjVkM2NiMGMyYWQwXkEyXkFqcGdeQXVyNjE5MjUyOTM@._V1_SX300.jpg"},
+        { title: "Happy",
+         genre: "Documentary, Drama, Family",
+         poster: "https://images-na.ssl-images-amazon.com/images/M/MV5BMjI1NTg1MzcwM15BMl5BanBnXkFtZTcwMjA2OTU4NQ@@._V1_SX300.jpg"},
+         {title:"Call Me by Your Name",
+        genre:"Drama, Romance",
+        poster:"https://m.media-amazon.com/images/M/MV5BNDk3NTEwNjc0MV5BMl5BanBnXkFtZTgwNzYxNTMwMzI@._V1_SX300.jpg"},
+        { title: "California",
+        genre: "Romance, Western",
+        poster: "https://m.media-amazon.com/images/M/MV5BNzhhMmM4M2ItMWZhOS00NDUzLTk5ZTAtMjVkM2NiMGMyYWQwXkEyXkFqcGdeQXVyNjE5MjUyOTM@._V1_SX300.jpg"},
+        { title: "Happy",
+         genre: "Documentary, Drama, Family",
+         poster: "https://images-na.ssl-images-amazon.com/images/M/MV5BMjI1NTg1MzcwM15BMl5BanBnXkFtZTcwMjA2OTU4NQ@@._V1_SX300.jpg"},
+        {title:"Call Me by Your Name",
+        genre:"Drama, Romance",
+        poster:"https://m.media-amazon.com/images/M/MV5BNDk3NTEwNjc0MV5BMl5BanBnXkFtZTgwNzYxNTMwMzI@._V1_SX300.jpg"},
+        { title: "California",
+        genre: "Romance, Western",
+        poster: "https://m.media-amazon.com/images/M/MV5BNzhhMmM4M2ItMWZhOS00NDUzLTk5ZTAtMjVkM2NiMGMyYWQwXkEyXkFqcGdeQXVyNjE5MjUyOTM@._V1_SX300.jpg"},
+        { title: "Happy",
+         genre: "Documentary, Drama, Family",
+         poster: "https://images-na.ssl-images-amazon.com/images/M/MV5BMjI1NTg1MzcwM15BMl5BanBnXkFtZTcwMjA2OTU4NQ@@._V1_SX300.jpg"},
+         {title:"Call Me by Your Name",
+        genre:"Drama, Romance",
+        poster:"https://m.media-amazon.com/images/M/MV5BNDk3NTEwNjc0MV5BMl5BanBnXkFtZTgwNzYxNTMwMzI@._V1_SX300.jpg"},
+        { title: "California",
+        genre: "Romance, Western",
+        poster: "https://m.media-amazon.com/images/M/MV5BNzhhMmM4M2ItMWZhOS00NDUzLTk5ZTAtMjVkM2NiMGMyYWQwXkEyXkFqcGdeQXVyNjE5MjUyOTM@._V1_SX300.jpg"},
+        { title: "Happy",
+         genre: "Documentary, Drama, Family",
+         poster: "https://images-na.ssl-images-amazon.com/images/M/MV5BMjI1NTg1MzcwM15BMl5BanBnXkFtZTcwMjA2OTU4NQ@@._V1_SX300.jpg"}
+
 ];
-    
+
 app.get("/", function(req, res){
     res.render("landing");
 });
 
 app.get("/movies", function(req, res){
-    res.render("movies",{movies:movies});
+    //get all movies from the DB;
+    // res.render("movies",{movies:movies});
+    Movie.find({}, function(err, allMovies){
+        if(err) {
+            console.log(err); //will do UI handling errors later;
+        } else {
+            res.render("movies", {movies:allMovies});
+        }
+    })
 });
 
 app.post("/movies", function(req, res){
@@ -59,13 +95,20 @@ app.post("/movies", function(req, res){
     var genre = req.body.genre;
     var poster = req.body.poster;
     var newMovie = {title: title, genre: genre, poster: poster}
-    movies.push(newMovie);
-    //redirect back to movies page
-    res.redirect("/movies");
+    //create a new movie and save to the databse
+    Movie.create(newMovie, function(err, newlyCreated){
+        if(err){
+            //send the user to the form page again and show them a message
+            console.log(err);
+        } else {
+            //redirect back to movies page
+            res.redirect('./movies');
+        }
+    })
 });
 
 app.get("/movies/new", function(req, res){
-   res.render("new.ejs"); 
+   res.render("new.ejs");
 });
 
 app.listen(process.env.PORT || 3000, process.env.IP, function(){
